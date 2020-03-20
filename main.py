48138d2532737f6b4eef9e1e1ecef3c9e7b62314
im'Omesh <suryaomesh@Omeshs-MacBook-Pro.local>
2020-03-20 15:34:45 -0400

    Updated the code with full functionality and requirements accoridng to the input type

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-s = input().split(' ')
-str1 = s[0]
-str2 = s[1]
-# import heapq as h
-from collections import defaultdict
-
-
-def makeDict(string):
-    str_map = defaultdict(int)
-    for char in string:
-        str_map[char] += 1
-    return str_map
-
-
-str_dict_1 = makeDict(str1)
-str_dict_2 = makeDict(str2)
-
+#author @imOmesh
+# s = input().split(' ')
+# str1 = s[0]
+# # str2 = s[1]
+# str1 = 'aaaaabbccdd'
+# str2 = 'yyyyyyxxxxx'
 
 def checkMapping(str_dict_1, str_dict_2):
     if not str_dict_1 and not str_dict_2:
+        # print ('true')
         return True
-    # elif not str_dict_1 and str_dict_2:
-    #     return False
+
     sorted_dict = sorted(list(str_dict_1.items()), key=lambda x: -x[1])
-    #    sorted_dict2 = sorted(list(str_dict_2.items()),key = lambda x: -x[1])
     if not sorted_dict:
+        # print ('false')
         return False
     lar_occ = sorted_dict[0]
     get_possible = [(item[1], item[0]) for item in str_dict_2.items() if item[1] >= lar_occ[1]]
     if not get_possible:
+        # print ('false')
         return False
-
     def getMin(elements):
         minimum = float('inf')
         for element in elements:
@@ -38,10 +27,8 @@ def checkMapping(str_dict_1, str_dict_2):
                 minimum = element[0]
         return out
 
-    # h.heapify(get_possible )
-    # element = h.heappop(get_possible)
+
     element = getMin(get_possible)
-    # for element in get_possible:
 
     new_str_dict_1 = str_dict_1.copy()
     del new_str_dict_1[lar_occ[0]]
@@ -51,8 +38,25 @@ def checkMapping(str_dict_1, str_dict_2):
         del new_str_dict_2[element[1]]
     temp = checkMapping(new_str_dict_1, new_str_dict_2)
     if temp:
+        # print('true')
         return True
+    # print ('false')
     return False
+if __name__ == '__main__':
+    import sys
+    s1 = sys.argv[1]
+    s2 = sys.argv[2]
+    from collections import defaultdict
 
+    def makeDict(string):
+        str_map = defaultdict(int)
+        for char in string:
+            str_map[char] += 1
+        return str_map
 
-print(checkMapping(str_dict_1, str_dict_2))
+    str_dict_1 = makeDict(s1)
+    str_dict_2 = makeDict(s2)
+    if checkMapping(str_dict_1, str_dict_2) :
+        print ('true')
+    else :
+        print ('false')
